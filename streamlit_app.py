3e0e35dce458554c232df897c36c274450d349aa
anlin37 <120582943+anlin37@users.noreply.github.com>
2022-12-14 18:37:24 +0100

    Update streamlit_app.py
    add smothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeat')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard Boiled Free-Range Egg')
+
+streamlit.title('Build Your Own Smoothie')
